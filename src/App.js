3fadc9dcbebb5b53f2657bbbc5a0b021b79a3fa9
himanshu shukla <hemi8085@gmail.com>
2024-03-18 22:32:24 +0530

    Time to make your first project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (userData) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList,userData];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
